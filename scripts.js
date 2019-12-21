4c854dc091218858aba2a013ec5967e72a268a21
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2019-12-21 09:27:22 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var x = document.getElementById("drop");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "visible";
+  }
+}
