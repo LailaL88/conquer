4efd6a483cc0d95c18f0f9cc93416f6616f6d1d8
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2019-12-22 17:02:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,3 +6,23 @@ function toggleMenu() {
     x.style.visibility = "visible";
   }
 }
+
+function smoothScroll(){
+	var element = document.querySelector("#section1");
+element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+
+function smoothScroll1(){
+var element = document.querySelector("#section2");
+element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+
+function smoothScroll2(){
+	var element = document.querySelector("#section3");
+element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+
+function smoothScroll3(){
+	var element = document.querySelector("#section7");
+element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
